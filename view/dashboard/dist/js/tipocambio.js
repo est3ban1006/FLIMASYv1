706454a795a07341e1a6614b9d7b5483b7077fdd
est3ban1006 <josuesteban14@gmail.com>
2020-12-14 23:40:00 -0600

    tipocambio

---
+++ view/dashboard/dist/js/tipocambio.js
@@ -0,0 +1,30 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function () {
+    consultarTipoCambio();
+
+});
+
+function consultarTipoCambio() {
+    $.ajax({
+        url: '../backend/controller/tipoCambio.php',
+        type: 'POST',
+        data: {
+            action: "consultarTipoCambio"
+        },
+        error: function () { //si existe un error en la respuesta del ajax
+            swal("Error", "Se presento un error al enviar la informacion", "error");
+        },
+        success: function (data) { //si todo esta correcto en la respuesta del ajax, la respuesta queda en el data
+            var json  = JSON.parse(data.trim());
+            alert(json.compra);
+            alert(json.venta);
+        }
+    });
+}
+
+
